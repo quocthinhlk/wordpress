589da359e8be66d8c29339ce9b281deb6f6a0eb0
Trương Quốc Thịnh <33736038+quocthinhlk@users.noreply.github.com>
2022-04-05 16:31:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 function my_custom_login_logo() {
     $custom_logo_id = get_theme_mod( 'custom_logo' );
     $logo_url = wp_get_attachment_image_src( $custom_logo_id , 'full' )[0];
-
     echo '<style type="text/css">
         body.login div#login h1 a {
             background-image: url('.$logo_url.');
@@ -11,6 +10,5 @@ function my_custom_login_logo() {
             width: auto;
         } 
     </style>';
-
-} 
+}
 add_action( 'login_enqueue_scripts', 'my_custom_login_logo' );
