e360659b5aa12185075f912f84df386d19bc6c4b
Trương Quốc Thịnh <33736038+quocthinhlk@users.noreply.github.com>
2022-04-05 16:23:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-// Change the Login Logo admin
+<!-- Change the Login Logo admin -->
+function my_custom_login_logo() {
+    $custom_logo_id = get_theme_mod( 'custom_logo' );
+    $logo_url = wp_get_attachment_image_src( $custom_logo_id , 'full' )[0];
+
+    echo '<style type="text/css">
+        body.login div#login h1 a {
+            background-image: url('.$logo_url.');
+            background-size: contain;
+            height: 100px;
+            width: auto;
+        } 
+    </style>';
+
+} 
+add_action( 'login_enqueue_scripts', 'my_custom_login_logo' );
